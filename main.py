bd58bab6675b6ab9834a5e9df6f7a595e437cfe8
kundan <kundan8789@gmail.com>
2026-04-17 12:40:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello Kundan")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name} 👋")
+
+if age >= 18:
+    print("You can vote 🗳️")
+else:
+    print("Wait for some years ⏳")
